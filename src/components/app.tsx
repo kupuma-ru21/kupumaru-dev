42ea92066e2690021562e5beed87f1927dbe71e4
kupuma-ru21 <kupumaru21@gmail.com>
2021-04-04 19:25:54 +0900

    feat: initial commit

---
+++ src/components/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import type { FC } from 'react';
+
+const App: FC = () => {
+  return (
+    <>
+      <h1>Hello TS-React!</h1>
+    </>
+  );
+};
+
+export default App;
